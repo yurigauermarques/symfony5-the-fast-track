fe084cc3306527e286265e96078dc90c9b11a890
Yuri Gauer Marques <yurigmarques@gmail.com>
2020-09-20 20:20:25 -0400

    Configura o painel adminstrativo

--- src/Entity/Comment.php
+++ src/Entity/Comment.php
@@ -48,6 +48,11 @@ class Comment
      */
     private $photoFilename;
 
+    public function __toString()
+    {
+        return (string) $this->getEmail();
+    }
+
     public function getId(): ?int
     {
         return $this->id;
