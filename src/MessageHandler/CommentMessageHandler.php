d3d1499bcc0742e89d0fa8159d8153caafe0bcff
Yuri Gauer Marques <yurigmarques@gmail.com>
2020-10-31 15:38:50 -0400

    Add Passo 26 Api Plataform

--- src/MessageHandler/CommentMessageHandler.php
+++ src/MessageHandler/CommentMessageHandler.php
@@ -9,7 +9,6 @@ use App\Repository\CommentRepository;
 use App\SpamChecker;
 use Doctrine\ORM\EntityManagerInterface;
 use Psr\Log\LoggerInterface;
-use Symfony\Bridge\Twig\Mime\NotificationEmail;
 use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
 use Symfony\Component\Messenger\MessageBusInterface;
 use Symfony\Component\Notifier\NotifierInterface;
@@ -60,16 +59,7 @@ class CommentMessageHandler implements MessageHandlerInterface
 
             $this->bus->dispatch($message);
         } elseif ($this->workflow->can($comment, 'publish') || $this->workflow->can($comment, 'publish_ham')) {
-            // $this->mailer->send(
-            //     (new NotificationEmail())
-            //         ->subject('New comment posted')
-            //         ->htmlTemplate('emails/comment_notification.html.twig')
-            //         ->from($this->adminEmail)
-            //         ->to($this->adminEmail)
-            //         ->context(['comment' => $comment])
-            // );
             $this->notifier->send(new CommentReviewNotification($comment), ...$this->notifier->getAdminRecipients());
-        // $this->notifier->send(new CommentReviewNotification($comment), ...$this->notifier->getAdminRecipients());
         } elseif ($this->workflow->can($comment, 'optimize')) {
             if ($comment->getPhotoFilename()) {
                 $this->imageOptimizer->resize($this->photoDir.'/'.$comment->getPhotoFilename());
